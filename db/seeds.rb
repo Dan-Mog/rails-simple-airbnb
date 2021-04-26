eb10ff372a3cf4ac6dd9b3728264a2158550940b
Daniella Mogilner <daniella.m@hotmail.co.uk>
2021-04-26 16:08:11 +0100

    started the air b nb challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Open Plan Studio Flat Manchester',
+  address: '19 Power Way M17 2RU',
+  description: 'A perfect space for a single or couple looking to explore Manchester',
+  price_per_night: 30,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Victorian Townhouse London',
+  address: '14 Priory Road London TW9 1DT',
+  description: 'Experience suburban London in this gorgeous period house',
+  price_per_night: 100,
+  number_of_guests: 7
+)
+
+Flat.create!(
+  name: 'Spacious 3 Bed House Birmingham',
+  address: '10 Douglas Road Birmingham B15 2RE',
+  description: 'A spacious 3 bed house in Birmingham complete with large garden and driveway',
+  price_per_night: 50,
+  number_of_guests: 5
+)
